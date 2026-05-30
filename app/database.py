86abab36c45e312eb623d8409d5026b853fea7f6
CamisM27 <camillacampos849@gmail.com>
2026-05-29 21:43:56 -0300

    Atualização da conexão com o banco

--- app/database.py
+++ app/database.py
@@ -1,27 +1,59 @@
 import os
 
 from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import (
+    declarative_base,
+    sessionmaker
+)
+
 
 load_dotenv()
 
-URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(URL)
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
+
+if not DATABASE_URL:
+
+    raise ValueError(
+        "DATABASE_URL não encontrada no .env"
+    )
+
+
+engine = create_engine(
+
+    DATABASE_URL,
+
+    pool_pre_ping=True,
+
+    echo=False
+)
+
 
 SessionLocal = sessionmaker(
+
     autocommit=False,
+
     autoflush=False,
+
     bind=engine
 )
 
+
 Base = declarative_base()
 
+
 def get_db():
+
     db = SessionLocal()
+
     try:
+
         yield db
+
     finally:
+
         db.close()
