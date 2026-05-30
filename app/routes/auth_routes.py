e74a71c4fedfe98eac0ba4b110c167c08dc2f1a2
CamisM27 <camillacampos849@gmail.com>
2026-05-29 21:45:17 -0300

    novos ajustes

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,12 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from app.schema.auth_schema import (
-    LoginRequest
-)
-
-from app.services.auth_service import (
-    login_service
-)
+from app.database import get_db
+from app.schema.auth_schema import LoginRequest
+from app.services.auth_service import login_service
 
 router = APIRouter(
     prefix="/api/v1/auth",
@@ -16,9 +13,11 @@ router = APIRouter(
 
 @router.post("/login")
 def login(
-    usuario: LoginRequest
+    usuario: LoginRequest,
+    db: Session = Depends(get_db)
 ):
 
     return login_service(
+        db,
         usuario
     )
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,20 +1,45 @@
-from app.auth.jwt_handler import criar_token
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
-from app.schema.auth_schema import (
-    LoginRequest
-)
+from app.auth.jwt_handler import criar_token
+from app.models.usuario_model import Usuario
+from app.schema.auth_schema import LoginRequest
 
 
 def login_service(
+    db: Session,
     usuario: LoginRequest
 ):
 
+    usuario_encontrado = db.query(
+        Usuario
+    ).filter(
+        Usuario.email == usuario.email
+    ).first()
+
+    if not usuario_encontrado:
+
+        raise HTTPException(
+            status_code=404,
+            detail="Usuário não encontrado"
+        )
+
+    if not usuario_encontrado.ativo:
+
+        raise HTTPException(
+            status_code=403,
+            detail="Usuário desativado"
+        )
+
     token = criar_token({
 
-        "email": usuario.email
+        "id_usuario":
+        usuario_encontrado.id_usuario,
+
+        "email":
+        usuario_encontrado.email
     })
 
     return {
-
         "access_token": token
     }
